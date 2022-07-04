d05b093ef77d0c85ce067aef7ef80be277257b54
Gabe Scholz <github@gabe.pizza>
2022-07-04 09:01:51 -0700

    Confirm this fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import * as React from "react";
 import { Button } from "ariakit";
 
+const useId = React["use" + "Id"] ?? (() => "1");
+
 function App() {
+  const id = useId();
+
   return <Button>Hello world</Button>;
 }
 
